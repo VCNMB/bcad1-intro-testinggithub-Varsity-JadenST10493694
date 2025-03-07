41512598edaf906ac889a81a5e559529968d52ad
Varsity-JadenST10493694 <ST10493694@vcconnect.edu.za>
2025-03-07 11:55:46 +0200

    OOP
    PULLED FROM NEW CLASSES

--- prjclasses/src/main/java/com/mycompany/prjclasses/Prjclasses.java
+++ prjclasses/src/main/java/com/mycompany/prjclasses/Prjclasses.java
@@ -11,6 +11,10 @@ package com.mycompany.prjclasses;
 public class Prjclasses {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        newClass nc = new newClass(); //creating an object of newClass
+        
+        nc.helloClass();// have to use variable name nc to communicate with the others
+        
+        System.out.println(nc.strSecondClassVariable);// output the variable from second class
     }
 }
